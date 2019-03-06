7de69efc897516f60fe34ba8c1ea229e827cf1d7
VividerAphid <trever.kress@gmail.com>
2019-03-06 13:48:07 -0500

    Stopped for better planning, added a plan file

---
+++ EncouroCon.Web/Controllers/DayCommentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EncouroCon.Web.Controllers
+{
+    public class DayCommentController : Controller
+    {
+        // GET: DayComment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
