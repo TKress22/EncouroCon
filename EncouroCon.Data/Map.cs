e7d5bb6b1fabb48b69ce46bdd4bc779c72d4a3ee
VividerAphid <trever.kress@gmail.com>
2019-03-06 16:30:43 -0500

    Final edits for the day

---
+++ EncouroCon.Data/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncouroCon.Data
+{
+    public class Map
+    {
+        public int MapID { get; set; }
+        public Guid OwnerID { get; set; }
+        public string PlanetData { get; set; }
+        public string Edges { get; set; }
+    }
+}
