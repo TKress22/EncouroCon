8949e2f90f07c3797462c424688e85b2362778d4
VividerAphid <trever.kress@gmail.com>
2019-03-06 11:22:56 -0500

    Basic DayComment stuff set up

---
+++ EncouroCon.Data/DayComment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncouroCon.Data
+{
+    public class DayComment
+    {
+        [Key]
+        public int CommentID { get; set; }
+
+        [Required]
+        public Guid OwnerID { get; set; }
+
+        [Required]
+        public bool WasGood { get; set; }
+
+        public string Reason { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
