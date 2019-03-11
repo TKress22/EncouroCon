2affbee40c073b275285d543395c8bbca2c39019
VividerAphid <trever.kress@gmail.com>
2019-03-11 10:15:29 -0400

    Working on viewing day stats

--- EncouroCon.Models/DayCommentListItem.cs
+++ EncouroCon.Models/DayCommentListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace EncouroCon.Models
 {
     public class DayCommentListItem
     {
+        public int CommentID { get; set; }
+        public bool WasGood { get; set; }
+        public string Reason { get; set; }
 
+        [Display (Name = "Day")]
+        public DateTimeOffset Created { get; set; }
     }
 }
--- EncouroCon.Web/Controllers/GameController.cs
+++ EncouroCon.Web/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EncouroCon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,18 @@ using System.Web.Mvc;
 
 namespace EncouroCon.Web.Controllers
 {
+    [Authorize]
     public class GameController : Controller
     {
         // GET: Game
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult DayStats()
+        {
+            var model = new DayCommentListItem[0];
+            return View(model);
         }
     }
 }
